221cee4b21c9f4d78ae462663dd2c6f5433ff8fd
serjik85 <tkachenko.s1985@gmail.com>
2020-09-05 19:31:33 +0300

    All requests in one file

--- js/getData.js
+++ js/getData.js
@@ -1,10 +1,10 @@
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 let token = 'perm:U2VyZ2V5X1RrYWNoZW5rbw==.NjAtMQ==.gONDyolCZrgbnHVP6UAekE85Nbxpm2';
 //let url = 'https://merehead.myjetbrains.com/youtrack/api/issues?query=project:"Testamex"&fields=has:("Need Tesing"),has("2 sprint"),id,summary,description';
-let url = 'https://merehead.myjetbrains.com/youtrack/api/issues/?query=Board%20Testamex%20Web:%20%7B2%20sprint%7D%20State:%20%7BNeed%20testing%7D';
+let url = 'https://merehead.myjetbrains.com/youtrack/api/issues/?query=Board%20Testamex%20Web:%20%7B3%20sprint%7D%20State:%20%7BNeed%20testing%7D';
+let urlIssue = 'https://merehead.myjetbrains.com/youtrack/api/issues/2-23516?fields=summary,description';
 
-
-(async function main() {
+async function getId() {
     try {
         let response = await fetch(proxyurl + url, {
             method: 'GET',
@@ -31,4 +31,29 @@ let url = 'https://merehead.myjetbrains.com/youtrack/api/issues/?query=Board%20T
     } catch (err) {
         //console.error(err);
     }
-})();
+};
+
+async function getData() {
+    try {
+        let responseId = await fetch(proxyurl + urlIssue, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+                'Authorization': 'Bearer ' + token,
+                //'fields': 'summary,description'
+            },
+        });
+        let resultId = await responseId.json();
+        //for (let i = 0; i <= result.length; i++)
+        console.log(resultId.description);
+        document.getElementById('issueData').textContent = (resultId.description)
+
+    } catch (err) {
+        //console.error(err);
+
+
+    }
+
+};
+getId();
+getData();
